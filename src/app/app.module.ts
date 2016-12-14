4552486e97cee782a6eff477754e0bc964dfc47e
Jimmy Cárdenas <jiacardenasor@unal.edu.co>
2016-12-14 08:13:23 -0500

    Creación de pipes y componentes para filtrar los productos
    - Se crea un pipe por cada filtro
    - Se crea un componente para cada filtro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
+import { CategoryComponent } from './filters/category/category.component';
+import { PriceComponent } from './filters/price/price.component';
+import { SearchComponent } from './filters/search/search.component';
+import { SortComponent } from './filters/sort/sort.component';
+import { TypeComponent } from './filters/type/type.component';
+import { CategoryPipe } from './pipes/category.pipe';
+import { MonedaPipe } from './pipes/moneda.pipe';
+import { PricePipe } from './pipes/price.pipe';
+import { SearchPipe } from './pipes/search.pipe';
+import { SortPipe } from './pipes/sort.pipe';
+import { TypePipe } from './pipes/type.pipe';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -17,7 +28,18 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductosComponent
+    ProductosComponent,
+    CategoryComponent,
+    PriceComponent,
+    SearchComponent,
+    SortComponent,
+    TypeComponent,
+    CategoryPipe,
+    MonedaPipe,
+    PricePipe,
+    SearchPipe,
+    SortPipe,
+    TypePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/moneda.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MonedaPipe } from './moneda.pipe';
+
+describe('MonedaPipe', () => {
+  it('create an instance', () => {
+    let pipe = new MonedaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
